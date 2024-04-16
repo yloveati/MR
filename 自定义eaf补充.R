5ec922a868f0a85ace3135bafed2e67bd4396949
ylovegyx <yloveati@126.com>
2024-04-17 01:29:40 +0800

    Add files via upload

---
+++ 自定义eaf补充.R
@@ -0,0 +1,134 @@
+snp_add_eaf <- function(dat, build = "37", pop = "EUR")
+{
+  stopifnot(build %in% c("37","38"))
+  stopifnot("SNP" %in% names(dat))
+  
+  # Create and get a url
+  server <- ifelse(build == "37","http://grch37.rest.ensembl.org","http://rest.ensembl.org")
+  pop <- paste0("1000GENOMES:phase_3:",pop)
+  
+  snp_reverse_base <- function(x)
+  {
+    x <- stringr::str_to_upper(x)
+    stopifnot(x %in% c("A","T","C","G"))
+    switch(x,"A"="T","T"="A","C"="G","G"="C")
+  }
+  
+  res_tab <- lapply(1:nrow(dat), function(i)
+  {
+    print(paste0("seaching for No.", i, " SNP"))
+    dat_i <- dat[i,]
+    
+    ext <- paste0("/variation/Homo_sapiens/",dat_i$SNP, "?content-type=application/json;pops=1")
+    url <- paste(server, ext, sep = "")
+    res <- httr::GET(url)
+    
+    # Converts http errors to R errors or warnings
+    httr::stop_for_status(res)
+    
+    # Convert R objects from JSON
+    res <- httr::content(res)
+    res_pop <- jsonlite::fromJSON(jsonlite::toJSON(res))$populations
+    
+    # Filter query results based on population set
+    res_pop <- try(res_pop[res_pop$population == pop,])
+    if("try-error" %in% class(res_pop))
+    {
+      print(paste0("There is not information for population ",pop))
+      queried_effect_allele <- "NR"
+      queried_other_allele <- "NR"
+      queried_eaf <- -1
+    }
+    else
+    {
+      if(nrow(res_pop)==0)
+      {
+        print(paste0("There is not information for population ",pop))
+        queried_effect_allele <- "NR"
+        queried_other_allele <- "NR"
+        queried_eaf <- -1
+      }
+      else
+      {
+        queried_effect_allele <- res_pop[1,"allele"][[1]]
+        queried_other_allele <- res_pop[2,"allele"][[1]]
+        queried_eaf <- res_pop[1,"frequency"][[1]]    
+      }
+    }
+    
+    effect_allele <- ifelse("effect_allele.exposure" %in% names(dat),
+                            dat_i$effect_allele.exposure,
+                            dat_i$effect_allele)
+    
+    other_allele <- ifelse("effect_allele.exposure" %in% names(dat),
+                           dat_i$other_allele.exposure,
+                           dat_i$other_allele)
+    
+    if("effect_allele.exposure" %in% names(dat))
+    {
+      name_output <- unique(c(names(dat), "eaf.exposure","reliability.exposure"))
+    }
+    else
+    {
+      name_output <- unique(c(names(dat), "eaf","reliability.exposure"))
+    }
+    
+    len_effect_allele <- nchar(effect_allele)
+    len_other_allele <- nchar(other_allele)
+    
+    if(len_effect_allele==1&len_other_allele==1)
+    {
+      if((queried_effect_allele==effect_allele & queried_other_allele==other_allele)|
+         (queried_effect_allele==other_allele & queried_other_allele==effect_allele))
+      {
+        dat_i$eaf.exposure <- ifelse(effect_allele == queried_effect_allele,
+                                     queried_eaf,
+                                     1-queried_eaf)
+        dat_i$eaf <- dat_i$eaf.exposure 
+        dat_i$reliability.exposure <- "high"
+      }
+      else
+      {
+        r_queried_effect_allele <- snp_reverse_base(queried_effect_allele)
+        r_queried_other_allele <- snp_reverse_base(queried_other_allele)
+        if((r_queried_effect_allele==effect_allele & r_queried_other_allele==other_allele)|
+           (r_queried_effect_allele==other_allele & r_queried_other_allele==effect_allele))
+        {
+          dat_i$eaf.exposure <- ifelse(effect_allele == r_queried_effect_allele,
+                                       queried_eaf,
+                                       1-queried_eaf)
+          dat_i$eaf <- dat_i$eaf.exposure 
+          dat_i$reliability.exposure <- "high"
+        }
+        else
+        {
+          dat_i$eaf.exposure <- ifelse(effect_allele == queried_effect_allele,
+                                       queried_eaf,
+                                       1-queried_eaf)
+          dat_i$eaf <- dat_i$eaf.exposure 
+          dat_i$reliability.exposure <- "low"
+        }
+      }
+    }
+    
+    else
+    {
+      # To identify the potential DEL/ INS
+      short_allele <- ifelse(len_effect_allele==1,
+                             effect_allele,
+                             other_allele)
+      short_allele_eaf <- ifelse(short_allele == queried_effect_allele, 
+                                 queried_eaf, 
+                                 1-queried_eaf)
+      dat_i$eaf.exposure <- ifelse(effect_allele == short_allele,
+                                   short_allele_eaf,
+                                   1-short_allele_eaf)
+      dat_i$eaf <- dat_i$eaf.exposure 
+      dat_i$reliability.exposure <- "low"
+    }
+    
+    dat_i[name_output]
+  })
+  
+  return(do.call(rbind, res_tab))
+}
